665269bd864c88ea99073a64ff967137aa73fbdc
harry <contact@harry.plus>
2026-07-04 16:21:07 +0530

    feat: Get events by block number

---
+++ src/Requests/GetEventsByBlockNumberRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Harryqt\Trongrid\Requests;
+
+use Saloon\PaginationPlugin\Contracts\Paginatable;
+
+class GetEventsByBlockNumberRequest extends BaseRequest implements Paginatable
+{
+    public function __construct(
+        protected readonly int $block_number,
+        protected readonly bool $only_confirmed = false,
+        protected readonly int $limit = 20,
+        protected readonly ?string $fingerprint = null,
+    ) {}
+
+    public function resolveEndpoint(): string
+    {
+        return "/blocks/$this->block_number/events";
+    }
+}
